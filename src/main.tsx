eb9ba4c4fb7ead335e2bfee0b988090a999ce743
Juliana <aragaopintojuli@gmail.com>
2025-02-17 22:20:38 -0300

    put helmet

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.css'
 import { Router } from './routes';
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>,
+  <HelmetProvider>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </HelmetProvider>
 )
